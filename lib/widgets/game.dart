d7fe4e8e6627e3392f42b832c4030f5f2b569bbd
Sandun Induranga <sandutharu40@gmail.com>
2025-03-24 22:45:42 +0530

    add XmlSpriteSheet class for XML-based sprite loading and refactor sprite initialization

--- lib/widgets/game.dart
+++ lib/widgets/game.dart
@@ -2,9 +2,10 @@ import 'dart:ui' as ui;
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame/extensions.dart';
 import 'package:flame/components.dart';
-import 'package:flame_kenney_xml/flame_kenney_xml.dart';
 import 'package:flutter/services.dart';
 import 'package:forge2d_game/widgets/background.dart';
+import 'package:xml/xml.dart';
+import 'package:xml/xpath.dart';
 
 class MyPhysicsGame extends Forge2DGame {
   MyPhysicsGame()
@@ -30,22 +31,51 @@ class MyPhysicsGame extends Forge2DGame {
     ].wait;
 
     aliens = XmlSpriteSheet(
-      image: aliensImage,
-      xml: await rootBundle.loadString('assets/spritesheet_aliens.xml'),
+      aliensImage,
+      await rootBundle.loadString('assets/spritesheet_aliens.xml'),
     );
 
     elements = XmlSpriteSheet(
-      image: elementsImage,
-      xml: await rootBundle.loadString('assets/spritesheet_elements.xml'),
+      elementsImage,
+      await rootBundle.loadString('assets/spritesheet_elements.xml'),
     );
 
     tiles = XmlSpriteSheet(
-      image: tilesImage,
-      xml: await rootBundle.loadString('assets/spritesheet_tiles.xml'),
+      tilesImage,
+      await rootBundle.loadString('assets/spritesheet_tiles.xml'),
     );
 
-    await world.add(Background(sprite: Sprite(backgroundImage as ui.Image)));
+    await world.add(Background(sprite: Sprite(backgroundImage)));
 
     return super.onLoad();
+  }
+}
+
+class XmlSpriteSheet {
+  XmlSpriteSheet(this.image, String xml) {
+    final document = XmlDocument.parse(xml);
+    for (final node in document.xpath('//TextureAtlas/SubTexture')) {
+      final name = node.getAttribute('name')!;
+      final x = double.parse(node.getAttribute('x')!);
+      final y = double.parse(node.getAttribute('y')!);
+      final width = double.parse(node.getAttribute('width')!);
+      final height = double.parse(node.getAttribute('height')!);
+      _rects[name] = Rect.fromLTWH(x, y, width, height);
+    }
+  }
+
+  final ui.Image image;
+  final _rects = <String, Rect>{};
+
+  Sprite getSprite(String name) {
+    final rect = _rects[name];
+    if (rect == null) {
+      throw ArgumentError('Sprite $name not found');
+    }
+    return Sprite(
+      image,
+      srcPosition: rect.topLeft.toVector2(),
+      srcSize: rect.size.toVector2(),
+    );
   }
 }
