a992f432d66229bec7a5ae393ad4827b7d815b99
Sandun Induranga <sandutharu40@gmail.com>
2025-03-24 23:46:16 +0530

    add BrickSize enum for defining various brick dimensions and random selection

--- lib/widgets/brick.dart
+++ lib/widgets/brick.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:flame/extensions.dart' as ui;
+
 const brickScale = 0.5;
 
 enum BrickType {
@@ -15,4 +17,21 @@ enum BrickType {
   const BrickType({required this.density, required this.friction});
 
   static BrickType get randomType => values[Random().nextInt(values.length)];
+}
+
+enum BrickSize {
+  size70x70(ui.Size(70, 70)),
+  size140x70(ui.Size(140, 70)),
+  size220x70(ui.Size(220, 70)),
+  size70x140(ui.Size(70, 140)),
+  size140x140(ui.Size(140, 140)),
+  size220x140(ui.Size(220, 140)),
+  size70x220(ui.Size(70, 220)),
+  size140x220(ui.Size(140, 220));
+
+  final ui.Size size;
+
+  const BrickSize(this.size);
+
+  static BrickSize get randomSize => values[Random().nextInt(values.length)];
 }
