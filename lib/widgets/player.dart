6f31f33b0e6b0bb9b12f2fbdce503e582a37f4de
Sandun Induranga <sandutharu40@gmail.com>
2025-03-25 23:45:29 +0530

    add Player class with drag functionality and sprite initialization

--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -1,5 +1,9 @@
 import 'dart:math';
 
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+
 const playerSize = 5.0;
 
 enum PlayerColor {
@@ -14,4 +18,37 @@ enum PlayerColor {
 
   static PlayerColor get randomColor =>
       PlayerColor.values[Random().nextInt(PlayerColor.values.length)];
+}
+
+class Player extends BodyComponent with DragCallbacks {
+  Player(Vector2 position, Sprite sprite)
+      : _sprite = sprite,
+        super(
+          renderBody: false,
+          bodyDef: BodyDef()
+            ..position = position
+            ..type = BodyType.static
+            ..angularDamping = 0.1
+            ..linearDamping = 0.1,
+          fixtureDefs: [
+            FixtureDef(CircleShape()..radius = playerSize / 2)
+              ..restitution = 0.4
+              ..density = 0.75
+              ..friction = 0.5,
+          ],
+        );
+
+  final Sprite _sprite;
+
+  @override
+  Future<void> onLoad() {
+    final spriteComponent = SpriteComponent(
+      sprite: _sprite,
+      size: Vector2.all(playerSize),
+      anchor: Anchor.center,
+      position: Vector2(0, 0),
+    );
+    add(spriteComponent);
+    return super.onLoad();
+  }
 }
