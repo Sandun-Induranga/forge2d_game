da1bf547147e91e646ee53d4aa060872a126babb
Sandun Induranga <sandutharu40@gmail.com>
2025-03-27 22:13:08 +0530

    add BodyComponentWithUserData class extending BodyComponent

---
+++ lib/widgets/body_component.dart
@@ -0,0 +1,13 @@
+import 'package:flame_forge2d/body_component.dart';
+
+class BodyComponentWithUserData extends BodyComponent {
+  BodyComponentWithUserData({
+    super.key,
+    super.bodyDef,
+    super.children,
+    super.fixtureDefs,
+    super.paint,
+    super.priority,
+    super.renderBody,
+  });
+}
